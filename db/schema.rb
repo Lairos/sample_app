5f8ee24b229b3b6a00e56fe5ebb2fc544d2f3354
Lairos <Rolland24F@gmail.com>
2014-11-13 15:57:44 +0800

    ch 11 done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111110541) do
+ActiveRecord::Schema.define(version: 20141113030507) do
+
+  create_table "microposts", force: true do |t|
+    t.text     "content",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "picture",    limit: 255
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at", using: :btree
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name",              limit: 255
